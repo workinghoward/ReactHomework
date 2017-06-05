0370f1e4778ada12fc7d9dbd373851686fd6329b
Oakley <otot333@gmail.com>
2017-06-06 03:15:34 +0800

    commit

--- src/CalculateCom.js
+++ src/CalculateCom.js
@@ -9,11 +9,18 @@ const divStyle = {
  var button2 = 0;
  var button3 = 0;
 class CalculateCom extends Component {   
-    test = 0;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            post: this.props.data,
+            bu1: '5'
+        };
+    }
     changebutton1Value(event)
     {
-        alert(event.target.value);
-        test= event.target.value;
+        return this.setState({bu1: this.state.bu1});
     }
 
     changebutton2Value(event)
@@ -27,13 +34,15 @@ class CalculateCom extends Component {
         alert(event.target.value);
         button3= event.target.value;
     }
+
     render() {
+        var tx = this.state.bu1;
         return (
             <div>
                 <div>
-                    <input type="text" style={divStyle} onChange={this.changebutton1Value}/>
+                    <input type="text" style={divStyle} onChange={this.changebutton1Value.bind(this)}/>
                     <input type="button" value="+" style={divStyle} />
-                    <input type="text" style={divStyle} />
+                    <input type="text"  value={tx} style={divStyle} />
                     <input type="button" value="-" style={divStyle} />
                     <input type="text" style={divStyle} />
                 </div>
